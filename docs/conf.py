261cd2df7e89ab27d7a54194309ad29b99214e51
fengshang1972 <51705206+fengshang1972@users.noreply.github.com>
2019-08-02 13:07:07 -0400

    Signed-off-by: fengshang1972 <51705206+fengshang1972@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -140,10 +140,10 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ('latexindex', 'epanet-manual.tex', 'EPANET USERS MANUAL',
+    (master_doc, 'epanet-manual.tex', 'EPANET USERS MANUAL',
      author, 'manual'),
 ]
-latex_appendices = ['A_units', 'B_error_messages', 'C_command_line_EPANET']
+#latex_appendices = ['A_units', 'B_error_messages', 'C_command_line_EPANET']
 numfig = True
 math_numfig = True
 
